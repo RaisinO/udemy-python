1ffdcc2ca5888fb6bb076f0e1b45f0e8975724a1
Richard Schure <schure1@gmail.com>
2017-06-02 09:04:06 -0700

    Working on Section 6

--- ex5-celcius-to-farenheit-to-file.py
+++ ex5-celcius-to-farenheit-to-file.py
@@ -4,15 +4,18 @@ def c_to_f(temp_c):
 
 Temperature = [10,-20,-289,100]
 
+file=open("converted-temps.txt",'w')
+file.close()
+
+for temp_c in Temperature:
+    if temp_c >= -273.15:
+        file=open("converted-temps.txt",'a')
+        file.write(str(c_to_f(temp_c))+"\n")
+    else:
+        file.close()
+
 for temp_c in Temperature:
     if temp_c >= -273.15:
         print (c_to_f(temp_c),"°F")
-
-        file=open("converted-temps.txt",'w')
-        for temp_c in Temperature:
-            c_to_f(temp_c)
-            file.write(str(temp_c)+"\n")
-            file.close()
-
     else:
         print("Temperature cannot be below absolute zero (-273.15 °C)!")
